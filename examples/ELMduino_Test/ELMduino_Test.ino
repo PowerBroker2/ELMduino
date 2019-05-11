d674a63f7048f3d6cca4e1256a7972f3aac74e65
PB2 <ltbergeron@gmail.com>
2019-05-11 17:37:51 -0400

    Add files via upload

--- examples/ELMduino_Test/ELMduino_Test.ino
+++ examples/ELMduino_Test/ELMduino_Test.ino
@@ -3,13 +3,17 @@
 ELM327 myELM327;
 
 float value;
+uint64_t currentTime = millis();
+uint64_t previousTime = currentTime;
+uint16_t samplePeriod = 200;
 
 void setup()
 {
   Serial.begin(115200);
   while(!Serial);
   
-  myELM327.begin(Serial);
+  if(!myELM327.begin(Serial))
+    Serial.println("Couldn't connect to ELM327");
 
   if(!myELM327.queryPID(SERVICE_01, VEHICLE_SPEED, 2, value))
     Serial.println("\tTimeout");
@@ -19,5 +23,12 @@ void setup()
 
 void loop()
 {
-  
+  currentTime = millis();
+  if((currentTime - previousTime) >= samplePeriod)
+  {
+    if(!myELM327.queryPID(SERVICE_01, VEHICLE_SPEED, 2, value))
+      Serial.println("\tTimeout");
+    else
+      Serial.println(value);
+  }
 }
