e21bd33608bb908960453a8d80dd67a680264e5c
PB2 <ltbergeron@gmail.com>
2019-05-13 16:51:26 -0400

    Update ELMduino_Test.ino

--- examples/ELMduino_Test/ELMduino_Test.ino
+++ examples/ELMduino_Test/ELMduino_Test.ino
@@ -2,27 +2,27 @@
 
 ELM327 myELM327;
 
-float speed;
+float rpm;
 uint64_t currentTime = millis();
 uint64_t previousTime = currentTime;
-uint16_t samplePeriod = 200;
+uint16_t samplePeriod = 80;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial1.begin(115200);
+  Serial3.begin(115200);
   
   delay(2000);
   
-  if(!myELM327.begin(Serial1))
+  if(!myELM327.begin(Serial3))
     Serial.println("Couldn't connect to ELM327");
 
-  if(!myELM327.querySpeed(speed))
+  if(!myELM327.queryRPM(rpm))
   {
-    //Serial.println("\tTimeout");
+    Serial.println("\tTimeout");
   }
   else
-    Serial.println(speed);
+    Serial.print("RPM: "); Serial.println(rpm);
 }
 
 void loop()
@@ -30,11 +30,13 @@ void loop()
   currentTime = millis();
   if((currentTime - previousTime) >= samplePeriod)
   {
-    if(!myELM327.querySpeed(speed))
+    previousTime = currentTime;
+    
+    if(!myELM327.queryRPM(rpm))
     {
-      //Serial.println("\tTimeout");
+      Serial.println("\tTimeout");
     }
     else
-      Serial.println(speed);
+      Serial.print("RPM: "); Serial.println(rpm);
   }
 }
