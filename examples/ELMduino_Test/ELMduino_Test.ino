832290ebc3b03465ea2dbbe1a523726002ff7a3a
PB2 <ltbergeron@gmail.com>
2019-05-21 18:33:39 -0400

    Added LED display stuff

--- examples/ELMduino_Test/ELMduino_Test.ino
+++ examples/ELMduino_Test/ELMduino_Test.ino
@@ -1,4 +1,3 @@
-#include <Adafruit_PWMServoDriver.h>
 #include "ELMduino.h"
 
 
@@ -7,15 +6,13 @@
 const float KMPH_MPH_CONVERT = 0.6213711922;
 const uint16_t MIN_RPM = 800;
 const uint16_t MAX_RPM = 5200;
-const uint16_t MAX_SHIELD_DUTY_CYCLE = 4095;
-const uint8_t MAX_NORMAL_DUTY_CYCLE = 255;
 const uint8_t SAMPLE_PERIOD = 100;
-const uint8_t BRIGHTNESS_PIN = 0;
+const uint8_t TENS_PLACE_START_PIN = 3;
+const uint8_t ONES_PLACE_START_PIN = 4;
 const uint8_t BAR_START_PIN = 2;
 
 
 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 ELM327 myELM327;
 
 enum fsm{
@@ -37,88 +34,114 @@ uint16_t normalDutyCycle;
 
 
 
-// 0 lit - 1 off
-uint8_t seven_seg_pix_map[10][7] = { //--------- 0
-                                    {0,  // E
-                                     1,  // G
-                                     0,  // D
-                                     0,  // F
-                                     0,  // C
-                                     0,  // A
-                                     0}, // B
-                                     //--------- 1
-                                    {0,  // E
-                                     1,  // G
-                                     1,  // D
-                                     0,  // F
-                                     1,  // C
-                                     1,  // A
-                                     1}, // B
-                                     //--------- 2
-                                    {1,  // E
-                                     0,  // G
-                                     0,  // D
-                                     0,  // F
-                                     0,  // C
-                                     0,  // A
-                                     1}, // B
-                                     //--------- 3
-                                    {0,  // E
-                                     0,  // G
-                                     0,  // D
-                                     0,  // F
-                                     1,  // C
-                                     0,  // A
-                                     1}, // B
-                                     //--------- 4
-                                    {0,  // E
-                                     0,  // G
-                                     1,  // D
-                                     0,  // F
-                                     1,  // C
-                                     1,  // A
-                                     0}, // B 
-                                     //--------- 5
-                                    {0,  // E
-                                     0,  // G
-                                     0,  // D
-                                     1,  // F
-                                     1,  // C
-                                     0,  // A
-                                     0}, // B
-                                     //--------- 6
-                                    {0,  // E
-                                     0,  // G
-                                     0,  // D
-                                     1,  // F
-                                     0,  // C
-                                     0,  // A
-                                     0}, // B
-                                     //--------- 7
-                                    {0,  // E
-                                     1,  // G
-                                     1,  // D
-                                     0,  // F
-                                     1,  // C
-                                     0,  // A
-                                     1}, // B
-                                     //--------- 8
-                                    {0,  // E
-                                     0,  // G
-                                     0,  // D
-                                     0,  // F
-                                     0,  // C
-                                     0,  // A
-                                     0}, // B
-                                     //--------- 9
-                                    {0,  // E
-                                     0,  // G
-                                     0,  // D
-                                     0,  // F
-                                     1,  // C
-                                     0,  // A
-                                     0}, // B
+uint8_t speed_led_pin_array[2][7] = { //--------- ones place
+                                     {51,  // A
+                                      50,  // B
+                                      47,  // C
+                                      48,  // D
+                                      49,  // E
+                                      53,  // F
+                                      52}, // G
+                                      //--------- tens place
+                                     {44,  // A
+                                      43,  // B
+                                      40,  // C
+                                      41,  // D
+                                      42,  // E
+                                      45,  // F
+                                      46}  // G
                                     };
+
+// 1 lit - 0 off
+uint8_t seven_seg_pix_map[11][7] = { //--------- 0
+                                    {1,  // A
+                                     1,  // B
+                                     1,  // C
+                                     1,  // D
+                                     1,  // E
+                                     1,  // F
+                                     0}, // G
+                                     //--------- 1
+                                    {0,  // A
+                                     0,  // B
+                                     0,  // C
+                                     0,  // D
+                                     1,  // E
+                                     1,  // F
+                                     0}, // G
+                                     //--------- 2
+                                    {1,  // A
+                                     0,  // B
+                                     1,  // C
+                                     1,  // D
+                                     0,  // E
+                                     1,  // F
+                                     1}, // G
+                                     //--------- 3
+                                    {1,  // A
+                                     0,  // B
+                                     0,  // C
+                                     1,  // D
+                                     1,  // E
+                                     1,  // F
+                                     1}, // G
+                                     //--------- 4
+                                    {0,  // A
+                                     1,  // B
+                                     0,  // C
+                                     0,  // D
+                                     1,  // E
+                                     1,  // F
+                                     1}, // G
+                                     //--------- 5
+                                    {1,  // A
+                                     1,  // B
+                                     0,  // C
+                                     1,  // D
+                                     1,  // E
+                                     0,  // F
+                                     1}, // G
+                                     //--------- 6
+                                    {1,  // A
+                                     1,  // B
+                                     1,  // C
+                                     1,  // D
+                                     1,  // E
+                                     0,  // F
+                                     1}, // G
+                                     //--------- 7
+                                    {1,  // A
+                                     0,  // B
+                                     0,  // C
+                                     0,  // D
+                                     1,  // E
+                                     1,  // F
+                                     0}, // G
+                                     //--------- 8
+                                    {1,  // A
+                                     1,  // B
+                                     1,  // C
+                                     1,  // D
+                                     1,  // E
+                                     1,  // F
+                                     1}, // G
+                                     //--------- 9
+                                    {1,  // A
+                                     1,  // B
+                                     0,  // C
+                                     1,  // D
+                                     1,  // E
+                                     1,  // F
+                                     1}, // G
+                                     //--------- blank
+                                    {0,  // A
+                                     0,  // B
+                                     0,  // C
+                                     0,  // D
+                                     0,  // E
+                                     0,  // F
+                                     0}, // G
+                                   };
 
 
 
@@ -127,14 +150,19 @@ void setup()
 {
   Serial.begin(115200);
   Serial3.begin(115200);
-  
+
+  // wait a bit for the ELM327 to come online
   delay(2000);
 
-  pwm.begin();
-  pwm.setPWMFreq(490);
-  Wire.setClock(400000);
-  
-  myELM327.begin(Serial3);
+  // initialize all LEDs in display
+  setupLEDs();
+
+  // connect to ELM327
+  while(!myELM327.begin(Serial3))
+  {
+    Serial.println("Couldn't connect to ELM327, trying again...");
+    delay(1000);
+  }
 }
 
 
@@ -142,6 +170,7 @@ void setup()
 
 void loop()
 {
+  // only query the ELM327 every so often
   currentTime = millis();
   if((currentTime - previousTime) >= SAMPLE_PERIOD)
   {
@@ -151,7 +180,10 @@ void loop()
     {
       case get_rpm:
         if(myELM327.queryRPM(rpm))
+        {
+          rpm = rpm / 4.0; // necessary conversion factor based off OBD-II standard
           updateLEDs();
+        }
         else
           Serial.println("\tTimeout");
         state = get_speed;
@@ -169,6 +201,24 @@ void loop()
         break;
     }
   }
+
+  // extra processing here:
+}
+
+
+
+
+void setupLEDs()
+{
+  for(uint8_t segment = 0; segment < 2; segment++)
+    for(uint8_t pin = 0; pin < 7; pin++)
+      pinMode(speed_led_pin_array[segment][pin], OUTPUT);
+
+  // blank out display
+  updateSevenSeg(0, 10);
+  updateSevenSeg(1, 10);
+  
+  return;
 }
 
 
@@ -182,9 +232,10 @@ void updateLEDs()
   Serial.print(rpm); Serial.print(" "); Serial.println(speed_mph);
 
   tensPlace = (uint8_t)speed_mph / 10;
+  if(tensPlace == 0)
+    tensPlace = 10; // this will cause a blank to be sent to the display
+    
   onesPlace = (uint8_t)(speed_mph + 0.5) % 10; // add 0.5 and type-cast in order to propperly round float
-
-  updateBrightness();
 
   updateSevenSeg(0, onesPlace);
   updateSevenSeg(1, tensPlace);
@@ -197,31 +248,14 @@ void updateLEDs()
 
 
 
-void updateBrightness()
-{
-  uint16_t brightness = analogRead(BRIGHTNESS_PIN);
-
-  brightness = 0; // for testing only - comment out once pot is installed
-  
-  shieldDutyCycle = map(brightness, 0, 1023, 0, MAX_SHIELD_DUTY_CYCLE);
-  normalDutyCycle = map(brightness, 0, 1023, 0, MAX_NORMAL_DUTY_CYCLE);
-  
-  return;
-}
-
-
-
-
 void updateSevenSeg(uint8_t segNum, uint8_t value)
 {
-  uint8_t startPin = segNum * 7;
-
-  for(uint8_t pwmPin = startPin; pwmPin < (startPin + 7); pwmPin++)
+  for(uint8_t i = 0; i < 7; i++)
   {
-    if(seven_seg_pix_map[value][pwmPin - startPin])
-      pwm.setPin(pwmPin, MAX_SHIELD_DUTY_CYCLE); // segment set high (off)
+    if(seven_seg_pix_map[value][i])
+      digitalWrite(speed_led_pin_array[segNum][i], LOW);
     else
-      pwm.setPin(pwmPin, shieldDutyCycle); // segment set low-ish (on)
+      digitalWrite(speed_led_pin_array[segNum][i], HIGH);
   }
   
   return;
@@ -232,26 +266,9 @@ void updateSevenSeg(uint8_t segNum, uint8_t value)
 
 void updateBar(uint16_t rpm)
 {
-  uint8_t BAR_END_PIN = BAR_START_PIN + 10;
-  uint8_t barValue = (uint8_t)(mapFloat(rpm, MIN_RPM, MAX_RPM, BAR_START_PIN, BAR_END_PIN) + 0.5); // add 0.5 and type-cast in order to propperly round float
-  
-  for(uint8_t barPin = BAR_START_PIN; barPin < BAR_END_PIN; barPin++)
-  {
-    if(barPin >= barValue)
-      analogWrite(barPin, normalDutyCycle); // segment set low-ish (on)
-    else
-      analogWrite(barPin, MAX_NORMAL_DUTY_CYCLE); // segment set high (off)
-  }
+  // todo
   
   return;
-}
-
-
-
-
-float mapFloat(long x, long in_min, long in_max, long out_min, long out_max)
-{
- return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
 }
 
 
