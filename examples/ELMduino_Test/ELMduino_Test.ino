dc3cba3959cfe09c11d97fd9cdee2fb938f4f1b0
PB2 <gitstuff2@gmail.com>
2019-11-11 15:12:39 -0500

    Update ELMduino_Test.ino

--- examples/ELMduino_Test/ELMduino_Test.ino
+++ examples/ELMduino_Test/ELMduino_Test.ino
@@ -11,7 +11,7 @@
 
 const uint16_t MIN_RPM              = 700;
 const uint16_t MAX_RPM              = 3500;
-const uint8_t  SAMPLE_PERIOD        = 100;
+const uint16_t SAMPLE_PERIOD        = 1000;
 const uint8_t  TENS_PLACE_START_PIN = 3;
 const uint8_t  ONES_PLACE_START_PIN = 4;
 const uint8_t  BAR_START_PIN        = 2;
