8af61df6dcefa2149590aed2f13252e07ce394fa
PB2 <gitstuff2@gmail.com>
2020-02-10 17:31:20 -0500

    Update and rename examples/ELMduino_Test/ELMduino_Test.ino to examples/ESP32_test/ESP32_test.ino

---
+++ examples/ESP32_test/ESP32_test.ino
@@ -0,0 +1,57 @@
+#include "BluetoothSerial.h"
+#include "ELMduino.h"
+
+
+#define ELM_PORT SerialBT
+#define ESP_BLUETOOTH_NAME "ESP32"
+
+
+BluetoothSerial SerialBT;
+ELM327 myELM327;
+
+
+uint32_t rpm = 0;
+
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  Serial.begin(115200);
+  ELM_PORT.begin(ESP_BLUETOOTH_NAME, true);
+
+  Serial.println("Attempting to connect to ELM327...");
+
+  if (!ELM_PORT.connect("OBDII"))
+  {
+    Serial.println("Couldn't connect to OBD scanner");
+    while (1);
+  }
+
+  myELM327.begin(ELM_PORT);
+
+  Serial.println("Connected to ELM327");
+  Serial.println("Ensure your serial monitor line ending is set to 'Carriage Return'");
+  Serial.println("Type and send commands/queries to your ELM327 through the serial monitor");
+  Serial.println();
+}
+
+
+void loop()
+{
+  float tempRPM = myELM327.rpm();
+
+  if (myELM327.status == ELM_SUCCESS)
+  {
+    rpm = (uint32_t)tempRPM;
+    Serial.print("RPM: "); Serial.println(rpm);
+  }
+  else
+  {
+    Serial.print(F("\tERROR: "));
+    Serial.println(myELM327.status);
+    delay(100);
+  }
+}
+
