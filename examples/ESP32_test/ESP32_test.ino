0e84a69046569171d10ab4d46aaa995e120d31e7
PB2 <gitstuff2@gmail.com>
2020-04-22 13:17:56 -0400

    Update ESP32_test.ino

--- examples/ESP32_test/ESP32_test.ino
+++ examples/ESP32_test/ESP32_test.ino
@@ -1,60 +1,53 @@
 #include "BluetoothSerial.h"
-#include "ELMduino.h"
-
-
-#define ELM_PORT SerialBT
-#define ESP_BLUETOOTH_NAME "ESP32"
 
 
 BluetoothSerial SerialBT;
-ELM327 myELM327;
 
 
-uint32_t rpm = 0;
+#define DEBUG_PORT Serial
+#define ELM_PORT   SerialBT
 
 
 void setup()
 {
-#if LED_BUILTIN
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-#endif
+  digitalWrite(LED_BUILTIN, HIGH);
 
-  Serial.begin(115200);
-  ELM_PORT.begin(ESP_BLUETOOTH_NAME, true);
+  DEBUG_PORT.begin(115200);
+  ELM_PORT.begin("ESP32test", true);
 
-  Serial.println("Attempting to connect to ELM327...");
+  DEBUG_PORT.println("Attempting to connect to ELM327...");
 
   if (!ELM_PORT.connect("OBDII"))
   {
-    Serial.println("Couldn't connect to OBD scanner - Phase 1");
-    while (1);
+    DEBUG_PORT.println("Couldn't connect to OBD scanner");
+    while(1);
   }
 
-  if (!myELM327.begin(ELM_PORT))
-  {
-    Serial.println("Couldn't connect to OBD scanner - Phase 2");
-    while (1);
-  }
-  
-  Serial.println("Connected to ELM327");
+  DEBUG_PORT.println("Connected to ELM327");
+  DEBUG_PORT.println("Ensure your serial monitor line ending is set to 'Carriage Return'");
+  DEBUG_PORT.println("Type and send commands/queries to your ELM327 through the serial monitor");
+  DEBUG_PORT.println();
 }
 
 
 void loop()
 {
-  float tempRPM = myELM327.rpm();
+  if(DEBUG_PORT.available())
+  {
+    char c = DEBUG_PORT.read();
 
-  if (myELM327.status == ELM_SUCCESS)
-  {
-    rpm = (uint32_t)tempRPM;
-    Serial.print("RPM: "); Serial.println(rpm);
+    DEBUG_PORT.write(c);
+    ELM_PORT.write(c);
   }
-  else
+
+  if(ELM_PORT.available())
   {
-    Serial.print(F("\tERROR: "));
-    Serial.println(myELM327.status);
-    delay(100);
+    char c = ELM_PORT.read();
+
+    if(c == '>')
+      DEBUG_PORT.println();
+
+    DEBUG_PORT.write(c);
   }
 }
-
