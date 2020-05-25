03609e67f615a282c4c3fe321a0a6639109d1922
PB2 <gitstuff2@gmail.com>
2020-05-25 14:38:42 -0400

    Create ESP8266_test.ino

---
+++ examples/ESP8266_test/ESP8266_test.ino
@@ -0,0 +1,77 @@
+#include <ESP8266WiFi.h>
+#include "ELMduino.h"
+
+
+const char* ssid = "WiFi_OBDII";
+const char* password = "your-password";
+
+
+//IP Adress of your ELM327 Dongle
+IPAddress server(192, 168, 0, 10);
+WiFiClient client;
+ELM327 myELM327;
+
+
+uint32_t rpm = 0;
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Connecting to ELM327 WiFi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid);
+  // WiFi.begin(ssid, password); //Use this line if your ELM327 has a password protected WiFi
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connected to Wifi");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (client.connect(server, 35000))
+    Serial.println("connected");
+  else
+  {
+    Serial.println("connection failed");
+    ESP.reset();
+  }
+
+  myELM327.begin(client);
+
+  Serial.println("Connected to ELM327");
+  Serial.println("Ensure your serial monitor line ending is set to 'Carriage Return'");
+  Serial.println("Type and send commands/queries to your ELM327 through the serial monitor");
+  Serial.println();
+}
+
+
+void loop()
+{
+  if(Serial.available())
+  {
+    char c = Serial.read();
+
+    Serial.write(c);
+    client.write(c);
+  }
+
+  if(client.available())
+  {
+    char c = client.read();
+
+    if(c == '>')
+      Serial.println();
+
+    Serial.write(c);
+  }
+}
