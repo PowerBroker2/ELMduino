c7491869afeff6f0e6a93cd7e16883b263173d24
Jim Whitelaw <jimwhitelaw@gmail.com>
2023-12-06 18:41:41 -0700

    Simplified code, implemented loop

--- examples/ESP32_Check_DTC/ESP32_Check_DTC.ino
+++ examples/ESP32_Check_DTC/ESP32_Check_DTC.ino
@@ -13,69 +13,13 @@ typedef enum
 
 BluetoothSerial SerialBT;
 ELM327 myELM327;
-dtc_states dtc_state = CHECKONE;
+dtc_states dtc_state = MILSTATUS;
 uint8_t numCodes = 0;
-
-void checkStatus()
-{
-    uint32_t dtcStatus = 0;
-    bool milStatusOn = 0;
-
-    dtcStatus = myELM327.monitorStatus();
-
-    if (myELM327.nb_rx_state == ELM_SUCCESS)
-    {
-        DEBUG_PORT.print("Response: ");
-        DEBUG_PORT.println(dtcStatus);
-        DEBUG_PORT.println((dtcStatus >> 24) & 0x80);
-        DEBUG_PORT.println((dtcStatus >> 24) - 0x80);
-
-        // We are only interested in the first byte of the response that
-        // encodes the MIL status and number of codes present
-        milStatusOn = ((dtcStatus >> 24) & 0x80) > 0;   // if MSB of the first byte is set (0x80 hex) then the light is on
-        numCodes = (uint8_t)((dtcStatus >> 24) - 0x80); // and the number of codes present is stored in the last 7 bits
-
-        DEBUG_PORT.print("MIL is on: ");
-        DEBUG_PORT.println(milStatusOn);
-        DEBUG_PORT.print("Number of Codes: ");
-        DEBUG_PORT.println(numCodes);
-    }
-    else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
-    {
-        myELM327.printError();
-    }
-}
-
-void getDTCCodes(uint8_t &numCodes)
-{
-    uint8_t dtcCodes = 0;
-
-    char *foundCodes[numCodes];
-
-    myELM327.currentDTCCodes(foundCodes, numCodes);
-
-    if (myELM327.nb_rx_state == ELM_SUCCESS)
-    {
-        DEBUG_PORT.print("Response: ");
-        DEBUG_PORT.println(dtcCodes);
-        DEBUG_PORT.println("Codes found: ");
-
-        size_t n = sizeof(foundCodes) / sizeof(foundCodes[0]);
-
-        for (int i = 0; i < n; i++)
-        {
-            DEBUG_PORT.println(foundCodes[i]);
-        }
-    }
-
-    else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
-    {
-        myELM327.printError();
-    }
-}
+uint32_t dtcStatus = 0;
 
 void setup()
 {
+
     DEBUG_PORT.begin(115200);
     // SerialBT.setPin("1234");
     ELM_PORT.begin("ArduHUD", true);
@@ -90,7 +34,7 @@ void setup()
 
     if (!myELM327.begin(ELM_PORT, true, 2000))
     {
-        DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 2");
+        DEBUG_PORT.println("ELM327 Couldn't connect to ECU - Phase 2");
         while (1)
             ;
     }
@@ -102,29 +46,68 @@ void loop()
 {
     switch (dtc_state)
     {
-    case CHECKONE:      // Returns only one code (if present) without first getting a code count
-        numCodes = 1;
-        getDTCCodes(numCodes);
-        numCodes = 0;
-        dtc_state = MILSTATUS;
-        break;
-
-    case MILSTATUS:     // Gets the number of current DTC codes present
-        checkStatus();
+    case CHECKONE:    // Force checking for a single code without first getting a code count
+        numCodes = 1; // Runs once after startup to test getDTCCodes(). Enable by setting initial dtc_state.
         dtc_state = DTCCODES;
         break;
 
-    case DTCCODES:      // If current DTC codes were found in previous request, get the codes
-        if (numCodes > 0) 
+    case MILSTATUS: // Gets the number of current DTC codes present
+
+        dtcStatus = myELM327.monitorStatus();
+
+        if (myELM327.nb_rx_state == ELM_SUCCESS)
         {
-            getDTCCodes(numCodes);
-            dtc_state = CHECKONE;
+            DEBUG_PORT.print("monitorStatus() response: ");
+            DEBUG_PORT.println(dtcStatus);
+            // DEBUG_PORT.println((dtcStatus >> 24) & 0x80);
+
+            // We are only interested in the first byte of the response that
+            // encodes the MIL status and number of codes present
+            // milStatusOn = ((dtcStatus >> 24) & 0x80) > 0;        // If MSB of the first byte is set (0x80 hex) then the light is on
+            numCodes = (uint8_t)((dtcStatus >> 24) - 0x80); // The number of codes present is stored in the last 7 bits
+            DEBUG_PORT.print("Number of Codes: ");
+            DEBUG_PORT.println(numCodes);
+            dtc_state = DTCCODES;
+        }
+        else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+        {
+            myELM327.printError();
+            dtc_state = DTCCODES;
+        }
+        break;
+
+    case DTCCODES: // If current DTC codes were found in previous request, thrn get the codes
+        if (numCodes > 0)
+        {
+            char *foundCodes[numCodes];
+
+            myELM327.currentDTCCodes(foundCodes, numCodes);
+            if (myELM327.nb_rx_state == ELM_SUCCESS)
+            {
+                DEBUG_PORT.print("Response to DTC request: ");
+                DEBUG_PORT.println(myELM327.response);
+                DEBUG_PORT.println("Codes found: ");
+
+                // Get the actual number of codes returned in case it is different than expected (numCodes)
+                size_t n = sizeof(foundCodes) / sizeof(foundCodes[0]);
+
+                for (int i = 0; i < n; i++)
+                {
+                    DEBUG_PORT.println(foundCodes[i]);
+                }
+                dtc_state = MILSTATUS;
+                delay(10000); // Pause for 10 sec after successful fetch of DTC codes.
+            }
+
+            else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+            {
+                myELM327.printError();
+                dtc_state = MILSTATUS;
+            }
         }
         break;
 
     default:
         break;
     }
-
-    delay(10000);
 }
